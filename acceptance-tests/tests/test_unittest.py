e9618208b6252ebe326bc74ed8cd92c2af515f8d
itomaldonado <mo.maldonado@gmail.com>
2018-01-31 00:31:22 -0500

    moved tests to xpath selection

--- acceptance-tests/tests/test_unittest.py
+++ acceptance-tests/tests/test_unittest.py
@@ -22,7 +22,7 @@ class SimpleAndroidTests(unittest.TestCase):
             '/Users/itomaldonado/git/wolkops/MobileApp_Demo/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk'
         )
 
-        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        self.driver = webdriver.Remote('http://127.0.0.1:4723/wd/hub', desired_caps)
 
 
     def tearDown(self):
@@ -32,63 +32,60 @@ class SimpleAndroidTests(unittest.TestCase):
 
     def test_home_tab(self):
         # pause a moment, so xml generation can occur
-        sleep(60)
+        sleep(35)
 
-        # el = self.driver.find_element_by_accessibility_id("Home")
-        # self.assertIsNotNone(el)
-        # el = self.driver.find_element_by_accessibility_id("Welcome to Ionic!")
-        # self.assertIsNotNone(el)
-        # el = self.driver.find_element_by_accessibility_id("This starter project comes with simple tabs-based layout for apps that are going to primarily use a Tabbed UI.")
-        # self.assertIsNotNone(el)
-        el = self.driver.find_element_by_accessibility_id("home Home")
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Home"]')
         self.assertIsNotNone(el)
-        # el = self.driver.find_element_by_accessibility_id("information circle About")
-        # self.assertIsNotNone(el)
-        # el = self.driver.find_element_by_accessibility_id("contacts Contact")
-        # self.assertIsNotNone(el)
-        # el = self.driver.find_element_by_accessibility_id("New Paragraph added")
-        # self.assertIsNotNone(el)
-        self.assertTrue(True)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Welcome to Ionic!"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="This starter project comes with simple tabs-based layout for apps that are going to primarily use a Tabbed UI."]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="home Home"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="information circle About"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="contacts Contact"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="contacts Contact"]')
+        self.assertIsNotNone(el)
 
 
     def test_about_tab(self):
         # pause a moment, so xml generation can occur
-        sleep(5)
+        sleep(35)
         
-        #el = self.driver.find_element_by_accessibility_id("information circle About")
-        #el.click()
-        #sleep(1)
-        #el = self.driver.find_element_by_accessibility_id("About")
-        #self.assertIsNotNone(el)
-        #el = self.driver.find_element_by_accessibility_id("home Home")
-        #el.click()
-        #sleep(1)
-        #el = self.driver.find_element_by_accessibility_id("Home")
-        #self.assertIsNotNone(el)
-        self.assertTrue(True)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="information circle About"]')
+        el.click()
+        sleep(5)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="About"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="home Home"]')
+        el.click()
+        sleep(5)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Home"]')
+        self.assertIsNotNone(el)
 
 
     def test_contact_tab(self):
         # pause a moment, so xml generation can occur
-        sleep(5)
+        sleep(35)
         
-        #el = self.driver.find_element_by_accessibility_id("contacts Contact")
-        #el.click()
-        #sleep(1)
-        #el = self.driver.find_element_by_accessibility_id("Contact")
-        #self.assertIsNotNone(el)
-        #el = self.driver.find_element_by_accessibility_id("Follow us on Twitter")
-        #self.assertIsNotNone(el)
-        #el = self.driver.find_element_by_accessibility_id("ionic")
-        #self.assertIsNotNone(el)
-        #el = self.driver.find_element_by_accessibility_id("@ionicframework")
-        #self.assertIsNotNone(el)
-        #el = self.driver.find_element_by_accessibility_id("home Home")
-        #el.click()
-        #sleep(1)
-        #el = self.driver.find_element_by_accessibility_id("Home")
-        #self.assertIsNotNone(el)
-        self.assertTrue(True)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="contacts Contact"]')
+        el.click()
+        sleep(5)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Contact"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Follow us on Twitter"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="ionic"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="@ionicframework"]')
+        self.assertIsNotNone(el)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="home Home"]')
+        el.click()
+        sleep(5)
+        el = self.driver.find_element_by_xpath('//*[@content-desc="Home"]')
+        self.assertIsNotNone(el)
 
 
 if __name__ == '__main__':
